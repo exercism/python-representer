bec33ff64866bfb9f79a11fd75e83f57310dcb80
brocla <github@whidit.com>
2024-03-14 14:38:15 -0600

    Drop unit tests. Be explict about dictionaries.

--- test/example-uniondict-normalization/example_uniondict_normalization.py
+++ test/example-uniondict-normalization/example_uniondict_normalization.py
@@ -1,3 +1,68 @@
+""" Examples adapted from Mecha Munch Management"""
+def update_recipes_tuple(ideas, recipe_updates):
+    """Update the recipe ideas dictionary.
+    :param ideas: dict - The "recipe ideas" dict.
+    :param recipe_updates: tuple - tuple with updates for the ideas section.
+    :return: dict - updated "recipe ideas" dict.
+    """
+    
+     # Since this action updates the dict in place, 
+     # the dict then needs to be returned separately, otherwise it is a syntax error. 
+    ideas |= dict(recipe_updates)
+    return ideas
+    
+    
+def update_recipes_dict(ideas, recipe_updates):
+    """Update the recipe ideas dictionary.
+    :param ideas: dict - The "recipe ideas" dict.
+    :param recipe_updates: dict - dictionary with updates for the ideas section.
+    :return: dict - updated "recipe ideas" dict.
+    """
+    
+     # Since this action returns a *new* dict, it can go directly on the return line.
+    return dict(ideas) | dict(recipe_updates)
+    
+##Example Usage##
+ideas  = {'Banana Bread' : {'Banana': 1, 'Apple': 1, 'Walnuts': 1, 'Flour': 1, 'Eggs': 2, 'Butter': 1}}
+    
+recipe_updates_tuple= (('Banana Bread', {'Banana': 4,  'Walnuts': 2, 'Flour': 1, 'Eggs': 2, 'Butter': 1, 'Milk': 2, 'Eggs': 3}),)
+    
+recipe_update_dict= {'Banana Bread': {'Banana': 4,  'Walnuts': 2, 'Flour': 1, 'Eggs': 2, 'Butter': 1, 'Milk': 2, 'Eggs': 3}}
+    
+update_recipes_tuple(ideas, recipe_updates_tuple)
+   # {'Banana Bread': {'Banana': 4, 'Walnuts': 2, 'Flour': 1, 'Eggs': 3, 'Butter': 1, 'Milk': 2}}
+   
+update_recipes_dict(ideas, recipe_update_dict)
+   # {'Banana Bread': {'Banana': 4, 'Walnuts': 2, 'Flour': 1, 'Eggs': 3, 'Butter': 1, 'Milk': 2}}
+
+
+
+""" Example from `alphametrics` exercise """
+def assign(letters, selections, lefty, righty):
+    while letters:
+        new_selections = []
+
+        for selection in selections:
+            slc, choices = selection
+
+            if letters[0] in [lefty, righty]:
+                curr_choices = choices - set([0])
+
+            else:
+                curr_choices = choices
+
+            for item in curr_choices:
+                actual = slc | {letters[0]: item}  # combine two dictionaries
+                new_selections.append((actual, choices - set([item])))
+
+        selections = new_selections
+        letters = letters[1:]
+    return [slc for slc, _ in selections]
+
+
+
+
+
 """
  This exmple comes from ChatGPT
  Prompt: Write a python function that 
@@ -16,7 +81,7 @@ def merge_settings(default_settings, toml_file_path):
         toml_settings = tomlib.load(f)
 
     # Merge default settings with settings from TOML file
-    merged_settings = default_settings | toml_settings
+    merged_settings = dict(default_settings) | dict(toml_settings)
     
     return merged_settings
 
@@ -30,65 +95,3 @@ toml_file_path = 'settings.toml'  # Path to the TOML file
 
 final_settings = merge_settings(default_settings, toml_file_path)
 print("Final Settings:", final_settings)
-
-
-
-
-""" Examples from PEP584 - Add Union Operators To dict"""
-d = {'spam': 1, 'eggs': 2, 'cheese': 3}
-e = {'cheese': 'cheddar', 'aardvark': 'Ethel'}
-# d | e   # Representer fails, result must be assigned. # TODO Fix this ????
-# e | d   # Representer fails
-x = d | e   # {'spam': 1, 'eggs': 2, 'cheese': 'cheddar', 'aardvark': 'Ethel'}
-y = e | d   # {'cheese': 3, 'aardvark': 'Ethel', 'spam': 1, 'eggs': 2}
-d |= e  # {'spam': 1, 'eggs': 2, 'cheese': 'cheddar', 'aardvark': 'Ethel'}
-
-
-
-
-
-""" Examples from the Cpython unit tests"""
-a = {0: 0, 1: 1, 2: 1}
-b = {1: 1, 2: 2, 3: 3}
-
-c = a.copy()
-c |= b
-
-assert a | b == {0: 0, 1: 1, 2: 2, 3: 3}
-assert c == {0: 0, 1: 1, 2: 2, 3: 3}
-
-c = b.copy()
-c |= a
-
-assert b | a == {1: 1, 2: 1, 3: 3, 0: 0}
-assert c == {1: 1, 2: 1, 3: 3, 0: 0}
-
-c = a.copy()
-c |= [(1, 1), (2, 2), (3, 3)]
-
-assert c == {0: 0, 1: 1, 2: 2, 3: 3}
-
-
-
-
-""" Example from `alphametrics` exercise """
-def assign(letters, selections, lefty, righty):
-    while letters:
-        new_selections = []
-
-        for selection in selections:
-            slc, choices = selection
-
-            if letters[0] in [lefty, righty]:
-                curr_choices = choices - set([0])
-
-            else:
-                curr_choices = choices
-
-            for i in curr_choices:
-                actual = slc | {letters[0]: i}  # combine two dictionaries
-                new_selections.append((actual, choices - set([i])))
-
-        selections = new_selections
-        letters = letters[1:]
-    return [slc for slc, _ in selections]
